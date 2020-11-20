e12539d74b4c787bdb7d3e6f24614a74c791cd33
Chris Cheetham <chris@cheetham.com>
2020-11-20 10:17:09 -0500

    Use core dynamic logger for actuators [#47]

--- src/steeltoe-3.0.x--netcoreapp3.1--webapi--csharp/Program.cs
+++ src/steeltoe-3.0.x--netcoreapp3.1--webapi--csharp/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Azure.SpringCloud.Client;
 {{/azure-spring-cloud}}
 {{#actuator-or-dynamic-logger}}
-using Steeltoe.Extensions.Logging.DynamicSerilog;
+using Steeltoe.Extensions.Logging;
 {{/actuator-or-dynamic-logger}}
 {{#cloud-foundry}}
 using Steeltoe.Common.Hosting;
@@ -59,7 +59,7 @@ namespace {{Namespace}}
                 .UseAzureSpringCloudService()
 {{/azure-spring-cloud}}
 {{#actuator-or-dynamic-logger}}
-                .ConfigureLogging((context, builder) => builder.AddSerilogDynamicConsole())
+                .ConfigureLogging((context, builder) => builder.AddDynamicConsole())
 {{/actuator-or-dynamic-logger}}
                 .UseStartup<Startup>();
             return builder;
